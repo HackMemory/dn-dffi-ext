f44d273e8669819489b38ed6c9c81e55ceecae84
HackMemory <akper.aliev@mail.ru>
2017-04-08 17:34:49 +0300

    Update GetCursorPos.php

--- examples/GetCursorPos.php
+++ examples/GetCursorPos.php
@@ -1,7 +1,5 @@
 <?php
 
-function cursor_pos(){
-	$point = new DFFIStruct("POINT", ["int", "int"]);
-	DFFI::callFunction("user32", "int", "GetCursorPos", [$point], ["struct"]);
-	pre($point->getResponse());
-}
+$point = new DFFIStruct("POINT", ["int", "int"]);
+DFFI::callFunction("user32", "int", "GetCursorPos", [$point], ["struct"]);
+pre($point->getResponse());
