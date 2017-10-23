548427b82790384eda1047705ae94ed09e38ee41
HackMemory <akper.aliev@mail.ru>
2017-10-23 21:21:48 +0300

    Update WriteProcessMemory.php

--- examples/WriteProcessMemory.php
+++ examples/WriteProcessMemory.php
@@ -1,23 +1,26 @@
 <?php
 
+$user32 = new DFFI("user32");
+$kernel32 = new DFFI("kernel32");
+
 $all_access = 0x000F0000 | 0x00100000 | 0xFFFF;
 $base = 0x6FFAE0; // значение взять из консоли (victim.exe)
 $newValue = 100;
 
-$hwnd = DFFI::callFunction("user32", "int", "FindWindowA", [null, "Victim"], ["int", "string"]); //получаем хендл окна
+$hwnd = $user32->FindWindowA("int", [null, "Victim"], ["int", "string"]); //получаем хендл окна
 if($hwnd == 0){
 	alert("Victim not found");
 }else{
 	$pid_ref = new DFFIReferenceValue("int");
-	DFFI::callFunction("user32", "int", "GetWindowThreadProcessId", [$hwnd, $pid_ref], ["int", "reference"]);//получаем pid процесса
+	$user32->GetWindowThreadProcessId("int", [$hwnd, $pid_ref], ["int", "reference"]);//получаем pid процесса
 	$pid = $pid_ref->getValue();
 
-	$hOpen = DFFI::callFunction("kernel32", "int", "OpenProcess", [$all_access, false, $pid], ["int", "bool", "int"]);
+	$hOpen = $kernel32->OpenProcess("int", [$all_access, false, $pid], ["int", "bool", "int"]);
 	if(!$hOpen){
 		alert("Process error");
 	}else{
 		$newValue_ref = new DFFIReferenceValue("int", $newValue);
-		DFFI::callFunction("kernel32", "bool", "WriteProcessMemory",     //записываем значение
+		$kernel32->WriteProcessMemory("bool",
 			[$hOpen, $base, $newValue_ref, sizeof($newValue), null],
 			["int", "int", "reference", "int", "int"]
 		);
